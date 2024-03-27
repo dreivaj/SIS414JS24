b380d63de30e25eb9a0fa7ec65c11ccdd4238a6e
Lisbeth <dioswachuka@gmail.com>
2024-03-26 20:00:19 -0400

    Tarae2 Lisbeth Cuenca Mamani

--- Tarea2/script.js
+++ Tarea2/script.js
@@ -1,51 +1,59 @@
-function custom(suma, Andiv){
-    return suma/ndiv
-}
-function suma(a, b){
-    return a+b
-}
-let f = function (a, b){return a+b} 
-let f1 =(a+b)  => {return a+b}
+function calcular() {
+    let result = 0;
 
-let result=  custom(f1(7,9),2)
-let result = custom(f1, 2)
-console.log("Result f: ", f(7, 9))
-console.log("Result f1: ", f1(7, 9))
-console.log("Result: ", result)
-let array =  [8, 9, 4, 5, 6, 1]
-array.forEach(item => {
-    console.log(item)
-})
-// let btn = document.getElementById("btn")
-let = document.querySelectorAll(".btn")
-btns.forEach(btn =>{
-    btn.addbtnEventListener("click", ()=>{
-        let varA = Number(document.getElementById("a").value)
-        let varB = Number(document.getElementById("b").value)
-        let result  = varA + varB
-        alert("Result "+ result)
-    })
-}) 
-let  persona = [
-    {
-        nombre: "Lis",
-        apellido: "Cuenca",
-        edad: "20",
-        nombreCompleto: function (){
-            return this.nombre + " "+ this.apellido
-        }
-    },
-    {
-        nombre: "Ales",
-        apellido: "Michel",
-        edad: "21",
-        nombreCompleto: function (){
-            return this.nombre + " "+ this.apellido
-        }
+    let p1 = document.querySelector("input[name='p1']:checked");
+    if (p1) {
+        result += Number(p1.value);
     }
-]
-//console.log(persona.nombreCompleto)
-personas.forEach(persona =>
-    console.log(personas))
+
+    let p2 = document.querySelector("input[type='text']").value.trim().toLowerCase();
+    if (p2 === "diablo") {
+        result += 5;
     }
+
+    let capital = document.getElementById("capital").value;
+    result += Number(capital);
+
+    let color1 = document.getElementById("color1").checked;
+    let color2 = document.getElementById("color2").checked;
+    let color3 = document.getElementById("color3").checked;
+    let color4 = document.getElementById("color4").checked;
+
+    if (!color2 && !color3 && color1 && color4) {
+        result += 5;
+    }
+
+    let p5 = document.querySelector("input[name='p12']:checked");
+    if (p5) {
+        result += Number(p5.value);
+    }
+    
+    let raiz = document.getElementById("respuesta").value.trim().toLowerCase();
+    if (raiz === "4" || raiz === "cuatro") {
+        result += 5;
+    }
+    
+    let respuestas = document.getElementsByName("respuesta");
+    respuestas.forEach(respuesta => {
+        if (respuesta.checked && respuesta.value === "yuri gagarin") {
+            result += 5;
+        }
+    });
+    
+    let p8 = document.querySelector("input[name='p8']:checked");
+    if (p8) {
+        result += Number(p8.value);
+    }
+
+    let p9 = document.querySelector("input[name='p9']:checked");
+    if (p9) {
+        result += Number(p9.value);
+    }
+
+    let p10 = document.querySelector("input[name='p10']:checked");
+    if (p10) {
+       result += Number(p10.value);
+    }
+
+    document.getElementById("result").innerHTML = "Puntaje: " + result;
 }
