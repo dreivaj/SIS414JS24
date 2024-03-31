ec83cd3254571bfe4698ef963a1e34e5402800ac
Vivi-414 <Judithcondori687@gmail.com>
2024-03-30 20:06:01 -0400

    problemas solucionados

--- tarea1/script.js
+++ tarea1/script.js
@@ -1,15 +1,22 @@
-document.getElementById('calcularBtn').addEventListener('click', function() {
-    sumar();
-});
-
-function sumar() {
+document.getElementById('sumarBtn').addEventListener('click', function() {
+    var reTexto;
     var num1 = parseFloat(document.getElementById('num1').value);
     var num2 = parseFloat(document.getElementById('num2').value);
-    
+    var resultado = sumarNumeros(num1, num2);
+    if(resultado%2==0){
+        reTexto="par"
+    }else{
+        reTexto="impar"
+    }
+    document.getElementById('resultado').innerText = "La suma de " + num1 + " y " + num2 + " es: " + resultado;
+    document.getElementById('reTexto').innerText = "El numero es: "+reTexto;
+
+});
+
+function sumarNumeros(num1, num2) {
     if (!isNaN(num1) && !isNaN(num2)) {
-        var resultado = num1 + num2;
-        document.getElementById('resultado').innerHTML = "La suma es: " + resultado;
+        return num1 + num2;
     } else {
-        alert("Por favor ingrese números válidos.");
+        return "Por favor, ingrese números válidos.";
     }
 }
