a88ffddc7cbdfbdec358d42cd6f01e9fd389fe35
Lisbeth <dioswachuka@gmail.com>
2024-04-10 16:54:04 -0400

    L.C.M Tarea3

--- Tarea3/script.js
+++ Tarea3/script.js
@@ -26,7 +26,7 @@ getUsers();
 function validarEntradas(rowData){
     return rowData.ru != "" && rowData.ci != "" && rowData.nombreCompleto != "";
 }
-//OBTENER LOS USUARIOS DE LA BASE DE DATOS FIREBASE
+
 async function getUsers() {
     let data = await fetch("https://sis414g224-default-rtdb.firebaseio.com/users.json");
     let jsonData = await data.json();
@@ -42,7 +42,7 @@ async function getUsers() {
     }
     table.innerHTML = htmlRow;
 }
-//ENVIAR UN NUEVO USUARIO A LA BASE DE DATOS
+
 async function postData(rowData) {
     let data = await fetch("https://sis414g224-default-rtdb.firebaseio.com/users.json", {
         method: "POST",
@@ -53,7 +53,7 @@ async function postData(rowData) {
     });
     getUsers(); 
 }
-//ELIMINAR USUSARIO DE LA BASE DE DATOS
+ 
 async function deleteUser(id) {
     await fetch(`https://sis414g224-default-rtdb.firebaseio.com/users/${id}.json`, {
         method: "DELETE"
@@ -61,7 +61,17 @@ async function deleteUser(id) {
     getUsers();
 }
 
-//ACTUALIZAR LOS DATOS EDITADOS DEL USUARIO EN LA BASE DE DATOS
+
+async function editUser(id) {
+    const res = await fetch(`https://sis414g224-default-rtdb.firebaseio.com/users/${id}.json`);
+    const user = await res.json();
+    document.getElementById("ru").value = user.ru;
+    document.getElementById("ci").value = user.ci;
+    document.getElementById("nCompleto").value = user.nombreCompleto;
+    document.getElementById("send").value = "Actualizar";
+    document.getElementById("send").setAttribute("data-key", id);
+}
+
 async function updateUser(rowData) {
     const id = document.getElementById("send").getAttribute("data-key");
     await fetch(`https://sis414g224-default-rtdb.firebaseio.com/users/${id}.json`, {
@@ -73,14 +83,4 @@ async function updateUser(rowData) {
     });
     document.getElementById("send").value = "Enviar";
     getUsers();
-}
-// EDITAR LOS DATOS DE UN USUARIO
-async function editUser(id) {
-    const res = await fetch(`https://sis414g224-default-rtdb.firebaseio.com/users/${id}.json`);
-    const user = await res.json();
-    document.getElementById("ru").value = user.ru;
-    document.getElementById("ci").value = user.ci;
-    document.getElementById("nCompleto").value = user.nombreCompleto;
-    document.getElementById("send").value = "Actualizar";
-    document.getElementById("send").setAttribute("data-key", id);
 }
