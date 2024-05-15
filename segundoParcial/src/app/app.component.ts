d155efab8a70d88839d030b70c51c68e59a4d857
Caleb  Coronado <coronadorodrigo668@gmail.com>
2024-05-15 00:17:54 -0400

    create componentFooter

--- segundoParcial/src/app/app.component.ts
+++ segundoParcial/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { NavPageComponent } from './nav-page/nav-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, NavPageComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, NavPageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
